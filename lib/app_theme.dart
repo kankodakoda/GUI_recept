c25d2363558b40a6311b942baf2bae25c5c1deca
Dinodinasour <ulrika.osterlund98@gmail.com>
2025-04-09 15:44:14 +0200

    Lade till AppTheme class och implementerar i main

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -1,3 +1,15 @@
+import 'package:flutter/material.dart';
+
 class AppTheme {
-  //hello
+  //Spacing
+  static const double paddingTiny = 4.0;
+  static double paddingSmall = 8.0;
+  static const double paddingMediumSmall = 12.0;
+  static const double paddingMedium = 16.0;
+  static const double paddingLarge = 24.0;
+  static const double paddingHuge = 32.0;
+
+  //App color
+
+  static ColorScheme colorScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple,);
 }
