e495d09cb8449639c03570ea22d79229d3c36109
kankodakoda <sebastian@wernqvist.eu>
2025-05-07 14:48:46 +0200

    Jag pushar för lite

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lab2/widgets/control_panel.dart';
-import 'package:lab2/widgets/difficulty_control.dart';
-import 'package:lab2/widgets/ingredient_control.dart';
-import 'package:lab2/widgets/kitchen_control.dart';
-import 'package:lab2/widgets/price_control.dart';
 import 'package:lab2/widgets/recipe_area.dart';
-import 'package:lab2/widgets/time_control.dart';
-
-// Nu testar jag gpush
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
